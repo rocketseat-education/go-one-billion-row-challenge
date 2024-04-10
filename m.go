43785707089fd2da61eb8ba10ec27c7d020aec77
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-04-10 14:57:29 -0300

    1brc - Parte 2

--- m.go
+++ m.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type Measurement struct {
-	Nome  string
 	Min   float64
 	Max   float64
 	Sum   float64
@@ -21,11 +22,37 @@ func main() {
 	}
 	defer measurements.Close()
 
+	dados := make(map[string]Measurement)
+
 	scanner := bufio.NewScanner(measurements)
 	for scanner.Scan() {
 		rawData := scanner.Text()
 		semicolon := strings.Index(rawData, ";")
 		location := rawData[:semicolon]
-		temp := rawData[semicolon+1:]
+		rawTemp := rawData[semicolon+1:]
+
+		temp, _ := strconv.ParseFloat(rawTemp, 64)
+
+		measurement, ok := dados[location]
+
+		if !ok {
+			measurement = Measurement{
+				Min:   temp,
+				Max:   temp,
+				Sum:   temp,
+				Count: 1,
+			}
+		} else {
+			measurement.Min = min(measurement.Min, temp)
+			measurement.Max = max(measurement.Max, temp)
+			measurement.Sum += temp
+			measurement.Count++
+		}
+
+		dados[location] = measurement
+	}
+
+	for name, measurements := range dados {
+		fmt.Printf("%s: %#+v\n", name, measurements)
 	}
 }
